3359e1bc6235266f99eae44c0099e55fded8b399
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-06-17 21:39:59 -0300

    fist commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerar PDF</title>
+    <style>
+        .botao{
+            text-decoration: nome;
+            border: 1px solid tomato;
+            color: black;
+            background-color: tomato;
+            display: inline-block;
+            padding: 10px 30px;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+        }
+    </style>
+</head>
+<body>
+    <h1>Exemplo de PDF</h1>
+    <a href="relatorio-pdf.php" class="botao" > Relatorio</a>
+</body>
+</html>
